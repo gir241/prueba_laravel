e3a3ef916fd3fe0a059911e7305cb6748fa647ec
gir241 <marco.molina.hidalgo@gmail.com>
2021-06-25 23:36:30 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Stanky!
+                    Kyo
                 </div>
 
                 <div class="links">
